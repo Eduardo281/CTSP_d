829fdc4996ae6124a91c39d3973254a7bbeb0d54
Eduardo <edteixeira28@hotmail.com>
2024-04-27 16:24:37 -0300

    Moved export_results() to MiscUtils.py

--- InstancesUtils.py
+++ InstancesUtils.py
@@ -1,13 +1,8 @@
 import os
 import re
-import sys
 import json
-import datetime
-import platform
 
 import PATHS
-
-import gurobipy as gp
 
 def read_instance(instance_name):
     return json.load(open(os.path.join(PATHS.INSTANCES_FOLDER, instance_name), "r"))
@@ -51,35 +46,3 @@ def load_200_vertices_clustered_instances_list():
         item for item in os.listdir(PATHS.INSTANCES_FOLDER) 
             if pattern.match(item)
     ]
-
-def export_results(
-        model, 
-        datetime_on_filename=True):
-    data = dict()
-    
-    data["instance_name"] = model.data["instance_name"]
-    data["solver_alias"] = model.alias
-    data["python_version"] = sys.version
-    data["gurobi_version"] = "Gurobi " + ".".join([str(val) for val in gp.gurobi.version()])
-    data["platform"] = platform.platform()
-    data["datetime"] = datetime.datetime.now().isoformat()
-
-    if(model.model.SolCount > 0):
-        data["objective_value"] = model.model.ObjVal
-        data["runtime"] = model.model.Runtime
-        data["GAP"] = model.model.MIPGap
-        data["route"] = model.routeList
-        data["x_vars"] = {str(pair): model.x[pair].X for pair in model.x if model.x[pair].X > 0.5}
-        if(len(model.u) > 0):
-            data["u_vars"] = {str(index): model.u[index].X for index in model.u if model.u[index].X > 0.5}
-        if(len(model.y) > 0):
-            data["y_vars"] = {str(pair): model.y[pair].X for pair in model.y if model.y[pair].X > 0.5}
-
-    filename = data["instance_name"]
-    if(datetime_on_filename):
-        filename += "_" + datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
-    filename += ".json"
-
-    f = open(os.path.join(PATHS.RESULTS_FOLDER, filename), "w")
-    json.dump(data, f)
-    f.close()
--- MiscUtils.py
+++ MiscUtils.py
@@ -1,3 +1,13 @@
+import os
+import sys
+import json
+import datetime
+import platform
+
+import gurobipy as gp
+
+import PATHS
+
 from BasicModels import MTZ_CTSP_d_Model, GP_CTSP_d_Model, SSB_CTSP_d_Model, SST_CTSP_d_Model
 from ValidInequalitiesBaseClass import VI_MTZ_CTSP_d_Model, VI_GP_CTSP_d_Model, VI_SSB_CTSP_d_Model, VI_SST_CTSP_d_Model, VI_Ha_CTSP_d_Model
 
@@ -12,3 +22,35 @@ def create_solvers_aliases_dict():
         model.alias: model
         for model in AVAILABLE_MODELS_LIST
     }
+
+def export_results(
+        model, 
+        datetime_on_filename=True):
+    data = dict()
+    
+    data["instance_name"] = model.data["instance_name"]
+    data["solver_alias"] = model.alias
+    data["python_version"] = sys.version
+    data["gurobi_version"] = "Gurobi " + ".".join([str(val) for val in gp.gurobi.version()])
+    data["platform"] = platform.platform()
+    data["datetime"] = datetime.datetime.now().isoformat()
+
+    if(model.model.SolCount > 0):
+        data["objective_value"] = model.model.ObjVal
+        data["runtime"] = model.model.Runtime
+        data["GAP"] = model.model.MIPGap
+        data["route"] = model.routeList
+        data["x_vars"] = {str(pair): model.x[pair].X for pair in model.x if model.x[pair].X > 0.5}
+        if(len(model.u) > 0):
+            data["u_vars"] = {str(index): model.u[index].X for index in model.u if model.u[index].X > 0.5}
+        if(len(model.y) > 0):
+            data["y_vars"] = {str(pair): model.y[pair].X for pair in model.y if model.y[pair].X > 0.5}
+
+    filename = data["instance_name"]
+    if(datetime_on_filename):
+        filename += "_" + datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+    filename += ".json"
+
+    f = open(os.path.join(PATHS.RESULTS_FOLDER, filename), "w")
+    json.dump(data, f)
+    f.close()
