07ee67d0ff975e094bed27a54f3a93d2fbafa28a
Eduardo <edteixeira28@hotmail.com>
2024-04-27 15:54:54 -0300

    Added auto-create missing folders

--- PATHS.py
+++ PATHS.py
@@ -3,3 +3,13 @@ import os
 INSTANCES_FOLDER = os.path.join(".", "Instances")
 RESULTS_FOLDER = os.path.join(".", "Results")
 SOLVED_INSTANCES_FOLDER = os.path.join(".", "Solved_Instances")
+
+FOLDERS = [
+    INSTANCES_FOLDER,
+    RESULTS_FOLDER,
+    SOLVED_INSTANCES_FOLDER
+]
+
+for folder in FOLDERS:
+    if not os.path.exists(folder):
+        os.mkdir(folder)
