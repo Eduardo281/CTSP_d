e40ce72d50627c729fbfc49b659cf24478a34df2
Eduardo <edteixeira28@hotmail.com>
2025-02-28 09:39:24 -0300

    New one line docstrings for the load functions

--- InstancesUtils.py
+++ InstancesUtils.py
@@ -20,21 +20,21 @@ def read_instance(instance_name: str):
     return json.load(open(os.path.join(PATHS.INSTANCES_FOLDER, instance_name), "r"))
 
 def load_small_random_instances_list():
-    """Build and return a list with all the random small instances"""
+    """Build and return a list with all the small random instances filenames."""
     return [
         item for item in os.listdir(PATHS.INSTANCES_FOLDER) 
             if ("berlin52-R-" in item or "swiss42-R-" in item)
     ]
 
 def load_small_clustered_instances_list():
-    """Build and return a list with all the cluster small instances"""
+    """Build and return a list with all the small clustered instances filenames."""
     return [
         item for item in os.listdir(PATHS.INSTANCES_FOLDER) 
             if ("berlin52-C-" in item or "swiss42-C-" in item)
     ]
 
 def load_100_vertices_random_instances_list():
-    """Build and return a list with all the random 100 vertices instances"""
+    """Build and return a list with all the 100 vertices random instances filenames."""
     pattern = re.compile("kro.100-R-")
     return [
         item for item in os.listdir(PATHS.INSTANCES_FOLDER) 
@@ -42,7 +42,7 @@ def load_100_vertices_random_instances_list():
     ]
 
 def load_100_vertices_clustered_instances_list():
-    """Build and return a list with all the cluster 100 vertices instances"""
+    """Build and return a list with all the 100 vertices clustered instances filenames."""
     pattern = re.compile("kro.100-C-")
     return [
         item for item in os.listdir(PATHS.INSTANCES_FOLDER) 
@@ -50,7 +50,7 @@ def load_100_vertices_clustered_instances_list():
     ]
 
 def load_200_vertices_random_instances_list():
-    """Build and return a list with all the random 200 vertices instances"""
+    """Build and return a list with all the 200 vertices random instances filenames."""
     pattern = re.compile("kro.200-R-")
     return [
         item for item in os.listdir(PATHS.INSTANCES_FOLDER) 
@@ -58,7 +58,7 @@ def load_200_vertices_random_instances_list():
     ]
 
 def load_200_vertices_clustered_instances_list():
-    """Build and return a list with all the cluster 200 vertices instances"""
+    """Build and return a list with all the 200 vertices clustered instances filenames."""
     pattern = re.compile("kro.200-C-")
     return [
         item for item in os.listdir(PATHS.INSTANCES_FOLDER) 
